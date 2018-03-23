63454eb618377ee5e051be6cd782af98a47614da
elijah johnston <eli@millinno.com>
2018-03-23 18:08:19 +0800

    initial commit

---
+++ public/src/components/ComparisonLineChart.vue
@@ -0,0 +1,31 @@
+<script>
+  import VueCharts from 'vue-chartjs'
+  import { Line } from 'vue-chartjs'
+
+  export default {
+  extends: Line,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      datasets: [
+        {
+          label: '',
+          backgroundColor: '#f87979',
+          data: []
+        }
+      ]
+    })
+  }
+
+  methods: {
+    addData: function(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+    }
+  }
+}
+</script>

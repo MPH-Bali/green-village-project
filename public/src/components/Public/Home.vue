fd7eb3527d4ca3b17ed9f25db0631acaca65e683
elijah johnston <eli@millinno.com>
2018-03-24 22:16:01 +0800

    added a couple components

--- public/src/components/Public/Home.vue
+++ public/src/components/Public/Home.vue
@@ -27,24 +27,6 @@
       <v-flex xs12>
         <v-card>
           <v-card-text>
-            <v-avatar color="grey lighten-2" size="80px">
-              <v-icon size="50px">assignment_turned_in</v-icon>
-            </v-avatar>
-            <h1 class="headline mt-2">PICKUP SCHEDULE</h1>
-            <p>Pickups for Pererenan are scheduled as follow:</p>
-
-            <h1 class="title">Banajar 1:</h1>
-            <h1 class="mb-3 headline primary--text">Mon & Fri 9 AM</h1>
-
-            <h1 class="title">Banajar 2:</h1>
-            <h1 class="mb-3 headline primary--text">Tue & Thu 9 AM</h1>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-
-      <v-flex xs12>
-        <v-card>
-          <v-card-text>
             <h1 class="headline mt-2">Recycling Efficiency</h1>
             <line-chart :height="300" :chart-data="recyclingEfficiency" />
           </v-card-text>
@@ -95,6 +77,65 @@
           <v-card-text class="text-xs-center">
             <h1 class="title">Income</h1>
             <bar-chart :height="300" :chart-data="incomeData" :options="barChartOptions" />
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-card>
+          <v-card-text>
+            <v-avatar color="grey lighten-2" size="80px">
+              <v-icon size="50px">assignment_turned_in</v-icon>
+            </v-avatar>
+            <h1 class="headline mt-2">COLLECTION</h1>
+            <v-layout row>
+              <v-flex xs4 sm4 md4>
+                <h1 class="title">Banajar 1:</h1>
+                <h1 class="mb-3 headline primary--text">DRIVEN</h1>
+              </v-flex>
+              <v-flex xs4 sm4 md4>
+                <h1 class="title">Banajar 1:</h1>
+                <h1 class="mb-3 headline primary--text">COLLECTIONS</h1>
+              </v-flex>
+              <v-flex xs4 sm 4 md4>
+                <h1 class="title">Banajar 1:</h1>
+                <h1 class="mb-3 headline primary--text">PICKUPS</h1>
+              </v-flex>
+          </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-card>
+          <v-card-text>
+            <v-avatar color="grey lighten-2" size="80px">
+              <v-icon size="50px">assignment_turned_in</v-icon>
+            </v-avatar>
+            <h1 class="headline mt-2">TEAM</h1>
+            <p>Pickups for Pererenan are scheduled as follow:</p>
+            <v-container fluid grid-list-xl>
+              <v-layout row>
+                 <v-flex xs4 sm4 md4 style="align-self: center;">
+                  <v-avatar slot="activator">
+                    <img src="http://mph-bali.org/wp-content/uploads/2016/08/logo.png" alt="">
+                  </v-avatar>
+                </v-flex>
+                <v-flex xs4 sm4 md4>
+                  <p>firstlastname</p>
+                  <p>roleatfacility</p>
+                </v-flex>
+            </v-layout>
+
+          <v-flex xs12>
+            <v-card>
+              <v-card-text class="text-xs-center">
+                <h1 class="title">Working Hours</h1>
+                <bar-chart :height="300" :chart-data="recyclingEfficiency" :options="barChartOptions" />
+              </v-card-text>
+            </v-card>
+            </v-flex>
+            </v-container>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -204,6 +245,28 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.Made-Surname-Facilit {
+  width: 132px;
+  height: 42px;
+  font-family: IBMPlexSans;
+  font-size: 14px;
+  font-weight: 300;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.5px;
+  text-align: left;
+  color: #393939;
+}
+.Made-Surname-Facilit .text-style-1 {
+  font-size: 18px;
+  font-weight: 600;
+}
+
+</style>
 
 
 <!-- function addData(chart, label, data) {

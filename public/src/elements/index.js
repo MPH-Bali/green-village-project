32b0b9899f637fdb0233d51175506f62287be681
elijah johnston <eli@millinno.com>
2018-03-25 00:25:18 +0800

    all public facing nav working except for translation

--- public/src/elements/index.js
+++ public/src/elements/index.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import LineChart from './LineChart'
 import BarChart from './BarChart'
 import DoughnutChart from './DoughnutChart'
+import HorizontalBarChart from './HorizontalBarChart'
+
 
 Vue.component('line-chart', LineChart)
 Vue.component('bar-chart', BarChart)
 Vue.component('doughnut-chart', DoughnutChart)
+Vue.component('horizontal-bar-chart', HorizontalBarChart)
 

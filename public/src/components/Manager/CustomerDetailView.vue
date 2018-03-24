daf4cdab30f263df1461db644a5fdadd3c18917d
Zack Perdue <zack@ziggidycreative.com>
2018-03-25 00:01:11 +0800

    progress on customer detail screen

--- public/src/components/Manager/CustomerDetailView.vue
+++ public/src/components/Manager/CustomerDetailView.vue
@@ -1,6 +1,15 @@
 <template>
   <v-container grid-list-lg>
     <v-layout row wrap>
+      <v-layout row wrap align-center>
+        <v-flex sx6>
+          <v-btn @click="$router.go(-1)" flat>
+             <v-icon size="20px" color="blue darken-2">arrow_back</v-icon>
+             <span class="ml-1">Back</span>
+           </v-btn>
+        </v-flex>
+        <v-flex sx6></v-flex>
+      </v-layout>
       <v-flex xs12>
         <v-card>
           <v-layout row wrap>
@@ -13,23 +22,17 @@
             <v-flex md3 xs12 pa-3>
               <h3 class="mb-3">Type</h3>
               <p class="subheading">
-                {{ person.household }}
+                {{ houseType }}
               </p>
             </v-flex>
-            <v-flex md3 xs12 pa-3>
-              <h3 class="mb-3">Company</h3>
-              <p class="subheading">
-                {{ person.company }}
-              </p>
-            </v-flex>
-          </v-layout>
-          <v-layout row wrap>
             <v-flex md3 xs12 pa-3>
               <h3 class="mb-3">SMS/Call</h3>
               <p class="subheading">
                 {{ person.phone }}
               </p>
             </v-flex>
+          </v-layout>
+          <v-layout row wrap>
             <v-flex md3 xs12 pa-3>
               <h3 class="mb-3">Whatsapp</h3>
               <p class="subheading">
@@ -42,18 +45,18 @@
                 {{ person.email }}
               </p>
             </v-flex>
-          </v-layout>
-          <v-layout row wrap>
             <v-flex md3 xs12 pa-3>
               <h3 class="mb-3">Address</h3>
               <p class="subheading">
                 {{ person.address }}
               </p>
             </v-flex>
+          </v-layout>
+          <v-layout row wrap>
             <v-flex md3 xs12 pa-3>
               <h3 class="mb-3">Notes</h3>
               <p class="subheading">
-                -
+                {{ notes }}
               </p>
             </v-flex>
           </v-layout>
@@ -74,6 +77,79 @@
           <v-card-title primary-title>
             <h3 class="mb-0">Payment History</h3>
           </v-card-title>
+          <v-layout pa-2 row align-center>
+            <v-flex sm9>
+              <v-layout row align-center>
+                <v-flex sm3>
+                  <v-dialog
+                    ref="timestampModal"
+                    persistent
+                    v-model="timestampModal"
+                    lazy
+                    full-width
+                    width="290px"
+                    :return-value.sync="date">
+                    <v-text-field
+                      slot="activator"
+                      v-model="form.timestamp"
+                      prepend-icon="event"
+                      readonly
+                      class="accent" solo flat
+                      label="Date Time" />
+                    <v-date-picker v-model="form.timestamp">
+                      <v-spacer></v-spacer>
+                      <v-btn flat color="primary" @click="timestampModal = false">Cancel</v-btn>
+                      <v-btn flat color="primary" @click="$refs.timestampModal.save(timestamp)">OK</v-btn>
+                    </v-date-picker>
+                  </v-dialog>
+                </v-flex>
+                <v-flex sm3>
+                  <v-text-field
+                    type="number"
+                    class="accent" solo flat
+                    v-model="form.monthlyFee"
+                    flat label="Monthly Fee" />
+                </v-flex>
+                <v-flex sm3>
+                  <v-text-field
+                    type="number"
+                    v-model="form.totalPaid"
+                    class="accent" solo flat
+                    label="Total Paid" />
+                </v-flex>
+                <v-flex sm3>
+                  <v-dialog
+                    ref="paidUntilDialog"
+                    persistent
+                    v-model="paidUntilModal"
+                    lazy
+                    full-width
+                    width="290px"
+                    :return-value.sync="date">
+                    <v-text-field
+                      slot="activator"
+                      v-model="form.paidUntil"
+                      prepend-icon="event"
+                      readonly
+                      class="accent" solo flat
+                      label="Paid Until"></v-text-field>
+                    <v-date-picker v-model="form.paidUntil">
+                      <v-spacer></v-spacer>
+                      <v-btn flat color="primary" @click="paidUntilModal = false">Cancel</v-btn>
+                      <v-btn flat color="primary" @click="$refs.paidUntilDialog.save(paidUntil)">OK</v-btn>
+                    </v-date-picker>
+                  </v-dialog>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex sm3 row align-center>
+              <v-btn
+                :disabled="submitting"
+                :loading="submitting"
+                @click="addFee"
+                large block color="success">Collect Fee</v-btn>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
 
@@ -87,7 +163,7 @@
               <td class="text-xs-center">{{ $moment(props.item.timestamp).format('DD/MM/YYYY') }}</td>
               <td class="text-xs-center">{{ props.item.monthlyFee }}</td>
               <td class="text-xs-center">{{ props.item.totalPaid }}</td>
-              <td class="text-xs-center">{{ props.item.paidUntil }}</td>
+              <td class="text-xs-center">{{ $moment(props.item.paidUntil).format('DD/MM/YYYY') }}</td>
             </template>
           </v-data-table>
         </v-card>
@@ -106,8 +182,34 @@ export default {
       })
     this.$firestore.syncFees(this.$route.params.id)
   },
+  methods: {
+    addFee (e) {
+      e.preventDefault()
+
+      this.submitting = true
+      this.$firestore.add('fee', {
+        ...this.form,
+        personId: this.$route.params.id
+      }).then(() => {
+        this.submitting = false
+        this.form = {}
+      })
+    }
+  },
+  computed: {
+    houseType: function () {
+      return this.person.houseType ? this.person.houseType.name : 'n/a'
+    },
+    notes: function () {
+      return this.person.notes ? this.person.notes : '-'
+    }
+  },
   data () {
     return {
+      paidUntilModal: false,
+      timestampModal: false,
+      submitting: false,
+      form: {},
       person: {},
       headers: [
         { text: 'Date Time', value: 'timestamp', align: 'center' },
@@ -115,15 +217,7 @@ export default {
         { text: 'Total Paid', value: 'totalPaid', align: 'center' },
         { text: 'Paid Until', value: 'paidUntil', align: 'center' }
       ],
-      items: [
-        {
-          personId: 'XXcgSBLHHkGb2KrOTJ0y',
-          timestamp: new Date(),
-          monthlyFee: 50,
-          totalPaid: 200,
-          paidUntil: '2018-07-24T09:55:48.942Z'
-        }
-      ]
+      items: []
     }
   }
 }

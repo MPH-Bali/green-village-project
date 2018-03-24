effc765f570c79ac36d4b86198d2990c0fad2c70
Anton <anlopan@gmail.com>
2018-03-24 09:31:58 +0300

    Move calendar into NavigationHeader component, change icons

---
+++ public/src/components/Manager/Ui/NavigationHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout row wrap>  
+    <v-flex xs4 >
+      <v-flex xs12 text-xs-center pt-4>
+      <p class='left'>
+        <slot name="left">
+          <v-icon small color="blue darken-2">fas fa-arrow-left</v-icon> Back
+        </slot>
+      </p>
+      </v-flex>
+    </v-flex>
+    <v-flex xs4 >
+      <v-flex xs12 text-xs-center pt-4>
+        <p>
+          <v-icon color="primary">event</v-icon>
+          <span>{{ $moment(timestamp).format('ddd, DD MMM YYYY') }}</span>
+        </p>
+      </v-flex>
+    </v-flex>
+    <v-flex xs4 >
+      <slot name="right">
+      </slot>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        timestamp: new Date()
+      }
+    }
+  }
+</script>
+
+<style>
+.left {
+  text-align: left;
+}
+</style>
--- public/src/components/index.js
+++ public/src/components/index.js
@@ -5,8 +5,10 @@ import DailyLog from './Manager/DailyLog'
 import DailyLogHeader from './Manager/DailyLogHeader'
 import DeliveryForm from './Manager/DeliveryForm'
 import DeliveryTable from './Manager/DeliveryTable'
+import NavigationHeader from './Manager/Ui/NavigationHeader'
 
 Vue.component('daily-log', DailyLog)
 Vue.component('daily-log-header', DailyLogHeader)
 Vue.component('delivery-form', DeliveryForm)
 Vue.component('delivery-table', DeliveryTable)
+Vue.component('navigation-header', NavigationHeader)

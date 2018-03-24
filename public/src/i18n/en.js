c41ab380916a6cfb737789c388dec0fad4bf7ce9
Nick Sarafa <nicksarafa@gmail.com>
2018-03-24 10:40:00 +0800

    login && loginEnterCode screens strings init

--- public/src/i18n/en.js
+++ public/src/i18n/en.js
@@ -1,5 +1,20 @@
 export default {
   home: {
-    welcome: 'Welcome'
-  }
+    welcome: 'Welcome',
+  },
+  login: {
+    headerTitle: 'login',
+    instructions: 'to login, please enter phone number',
+    inputPlaceholder: 'type your phone number',
+    label: 'phone number',
+    button: 'continue',
+  },
+  loginEnterCode: {
+    headerTitle: 'login',
+    instructions: 'please enter the code we sent you via sms',
+    codeInputLabel: 'code',
+    codeInputPlaceholder: '012345',
+    resendCodeLink: 'resend code',
+    button: 'login',
+  },
 }

0858811ccc0ff525a89c12b8c1dfcd115c650cfd
Nick Sarafa <nicksarafa@gmail.com>
2018-03-25 15:08:03 +0800

    prettify dbHelper and seed.json

--- functions/dbHelper.js
+++ functions/dbHelper.js
@@ -1,42 +1,45 @@
 /* eslint-disable */
 
 function deleteCollection(db, collectionPath, batchSize) {
-	const query = db.collection(collectionPath);
+  const query = db.collection(collectionPath);
 
-	return new Promise((resolve, reject) => {
-		deleteQueryBatch(db, query, batchSize, resolve, reject);
-	});
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, query, batchSize, resolve, reject);
+  });
 }
 
 function deleteQueryBatch(db, query, batchSize, resolve, reject) {
-	let numDeleted = null;
-	return query.get()
-		.then((snapshot) => {
-			numDeleted = snapshot.size;
-			// When there are no documents left, we are done
-			if (snapshot.size === 0) {
-				return 0;
-			}
+  let numDeleted = null;
+  return query
+    .get()
+    .then(snapshot => {
+      numDeleted = snapshot.size;
+      // When there are no documents left, we are done
+      if (snapshot.size === 0) {
+        return 0;
+      }
 
-			// Delete documents in a batch
-			const batch = db.batch();
-			snapshot.docs.forEach((doc) => {
-				batch.delete(doc.ref);
-			});
+      // Delete documents in a batch
+      const batch = db.batch();
+      snapshot.docs.forEach(doc => {
+        batch.delete(doc.ref);
+      });
 
-			return batch.commit();
-		})
-		.then(() => {
-			if (numDeleted === 0) {
-				resolve();
-				return;
-			}
-			// Recurse on the next process tick, to avoid
-			// exploding the stack.
-			process.nextTick(() => deleteQueryBatch(db, query, batchSize, resolve, reject));
-		})
-		.catch(reject);
+      return batch.commit();
+    })
+    .then(() => {
+      if (numDeleted === 0) {
+        resolve();
+        return;
+      }
+      // Recurse on the next process tick, to avoid
+      // exploding the stack.
+      process.nextTick(() =>
+        deleteQueryBatch(db, query, batchSize, resolve, reject)
+      );
+    })
+    .catch(reject);
 }
 module.exports = {
-	deleteCollection
-}
+  deleteCollection
+};

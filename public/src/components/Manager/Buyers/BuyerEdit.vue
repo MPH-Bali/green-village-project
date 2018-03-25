aa8fcd828d8eda381ebd70c72de82fadf482a62d
Рома <roma219@yandex.ru>
2018-03-25 21:33:58 +0800

    cleanup

--- public/src/components/Manager/Buyers/BuyerEdit.vue
+++ public/src/components/Manager/Buyers/BuyerEdit.vue
@@ -150,7 +150,6 @@ export default {
       if (!this.id) this.formData.type = {}
       this.formData.type.buyer = true
       this.savePending = true
-      console.log(this.formData)
       await this.$firestore.save('person', this.formData)
       this.$emit('message', 'Buyer data saved', 'success')
       this.savePending = false

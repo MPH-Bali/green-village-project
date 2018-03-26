e515e8776f3943bd30470f57fc09237ecabb74c9
Shane <shane@mediashane.com>
2018-03-26 15:15:14 +0800

    updated with Nico's QA

--- public/src/components/Manager/AddStock/EditStockForm.vue
+++ public/src/components/Manager/AddStock/EditStockForm.vue
@@ -5,32 +5,24 @@
         <navigation-header />
         <v-layout row wrap>
           <v-flex xs6>
-            <v-text-field solo flat label="Weight" suffix="kg" type="number" v-model="formData.weight" />
+            <v-text-field solo flat class="accent" label="Weight" suffix="kg" type="number" v-model="formData.weight" />
           </v-flex>
           <v-flex xs6>
-            <v-select solo flat :items="['Compost', 'Plastics', 'Metals']" v-model="formData.type" label="Type" />
+            <v-select solo flat class="accent" :items="['Compost', 'Plastics', 'Metals']" v-model="formData.type" label="Type" />
           </v-flex>
           <v-flex xs12>
-            <v-text-field solo flat label="Comments" v-model="formData.comments" auto-grow multi-line/>
+            <v-text-field solo flat class="accent" label="Comments" v-model="formData.comments" auto-grow multi-line/>
           </v-flex>
 
           <v-flex xs4>
             <v-btn color="error" flat outline @click.stop="$router.go(-1)">Cancel</v-btn>
           </v-flex>
-          <v-flex xs4 text-xs-center>
-            <v-btn v-if="this.formData.id"
-                   color="error"
-                   depressed
-                  @click.stop="remove"
-                  :loading="deletePending">Delete
-            </v-btn>
-          </v-flex>
-          <v-flex xs4 text-xs-right>
+          <v-flex xs8 text-xs-right>
             <v-btn style="text-transform: capitalize"
                    v-if="this.formData.id"
                    depressed color="primary"
                   @click.stop="save"
-                  :loading="savePending">Save Delivery
+                  :loading="savePending">Update
             </v-btn>
             <v-btn style="text-transform: capitalize"
                    v-if="!this.formData.id"

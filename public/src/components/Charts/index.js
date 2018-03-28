4931dc8c90b0578a342c7080eac970d5c0ff7f08
nicolas.zahnd@ch.pwc.com <jollybrackets@users.noreply.github.com>
2018-03-29 02:47:17 +0800

    move charts

---
+++ public/src/components/Charts/index.js
@@ -0,0 +1,12 @@
+// ToDo: Do this dynamically in vue-loader (if possible)
+import Vue from 'vue'
+
+import LineChart from './LineChart'
+import BarChart from './BarChart'
+import DoughnutChart from './DoughnutChart'
+import HorizontalBarChart from './HorizontalBarChart'
+
+Vue.component('line-chart', LineChart)
+Vue.component('bar-chart', BarChart)
+Vue.component('doughnut-chart', DoughnutChart)
+Vue.component('horizontal-bar-chart', HorizontalBarChart)

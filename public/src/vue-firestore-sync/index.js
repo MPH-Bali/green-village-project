15338a32111489a038c24e9f1c0d1568ea003850
nicolas.zahnd@ch.pwc.com <jollybrackets@users.noreply.github.com>
2018-03-30 20:05:55 +0800

    added query & get support

--- public/src/vue-firestore-sync/index.js
+++ public/src/vue-firestore-sync/index.js
@@ -17,12 +17,16 @@ const initInstance = (Vue, db) => {
       sync (refs) {
         Object.keys(refs).forEach(key => {
           // Check if collection or document
-          const isCollection = !refs[key].parent
+          const isQuery = !refs[key].path
+          const isCollection = !isQuery && !refs[key].parent
+
           // First unsubscribe if the key already exists
           if (this.store[key]) this.store[key].unsubscribe()
 
           // Get collection name
-          const colId = isCollection ? refs[key].id : refs[key].parent.id
+          const colId = (isQuery && refs[key]._query.path.segments[0]) ||
+                        (isCollection && refs[key].id) ||
+                        refs[key].parent.id
           const colRef = db.collection(colId)
 
           // Set/reset store for this key
@@ -37,13 +41,20 @@ const initInstance = (Vue, db) => {
               add: (data) => colRef.add(data),
               remove: (id) => colRef.doc(id).delete(),
               update: (data) => colRef.doc(data.id).set({ ...data }),
-              save: (data) => data.id ? colRef.doc(data.id).set({ ...data }) : colRef.add(data)
+              save: (data) => data.id ? colRef.doc(data.id).set({ ...data }) : colRef.add(data),
+              get: async (id) => {
+                const cached = this.store[key].data.find(x => x.id === id)
+                if (cached) return cached
+
+                const doc = this.store[key].collection.ref.doc(id).get()
+                return { id: doc.id, ...doc.data() }
+              }
             }
           }
 
           Vue.set(this.store, 'key', this.store[key])
 
-          if (isCollection) {
+          if (isCollection || isQuery) {
             this.store[key].unsubscribe = refs[key].onSnapshot(snapshot => {
               this.store[key].data = []
               snapshot.forEach(doc => {

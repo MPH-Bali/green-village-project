9a610929db059d8b14d2fe5aea5da4b8b303f900
nicolas.zahnd@ch.pwc.com <jollybrackets@users.noreply.github.com>
2018-04-09 23:21:25 +0200

    expic refactor pt. 5

--- public/src/components/Manager/DailyLog/WorkerHours/WorkerHoursForm.vue
+++ public/src/components/Manager/DailyLog/WorkerHours/WorkerHoursForm.vue
@@ -7,7 +7,6 @@
           <v-flex xs12>
             <p class="body-2 mb-1">Worker name</p>
             <v-select solo flat :items="$store.workers.data"
-              @change="error = null"
               v-model="form.worker"
               item-text="name"
               item-value="id"
@@ -17,32 +16,12 @@
 
           <v-flex xs12 sm6>
             <p class='body-2 mb-0'>Time in</p>
-            <v-layout row wrap>
-              <v-flex xs7>
-                <v-select solo flat :items="hours" v-model="checkIn" class="accent" />
-              </v-flex>
-              <v-flex xs5>
-                <v-btn-toggle mandatory class="elevation-0" v-model="checkInPostfix">
-                  <v-btn outline large class="primary--text px-3" value="AM">AM</v-btn>
-                  <v-btn outline large class="primary--text px-3" value="PM">PM</v-btn>
-                </v-btn-toggle>
-              </v-flex>
-            </v-layout>
+            <time-picker v-model="form.checkIn" />
           </v-flex>
 
           <v-flex xs12 sm6>
             <p class='body-2 mb-0'>Time out</p>
-            <v-layout row wrap>
-              <v-flex xs7>
-                <v-select solo flat :items="hours" v-model="checkOut" class="accent" />
-              </v-flex>
-              <v-flex xs5>
-                <v-btn-toggle mandatory class="elevation-0" v-model="checkOutPostfix">
-                  <v-btn outline large class="primary--text px-3" value="AM">AM</v-btn>
-                  <v-btn outline large class="primary--text px-3" value="PM">PM</v-btn>
-                </v-btn-toggle>
-              </v-flex>
-            </v-layout>
+            <time-picker v-model="form.checkOut" />
           </v-flex>
 
           <v-flex xs12 mb-3>
@@ -81,8 +60,6 @@
 </template>
 
 <script>
-import moment from 'moment'
-
 export default {
   props: {
     id: { type: String, required: false }
@@ -90,24 +67,17 @@ export default {
   async created () {
     if (this.id) {
       this.form = await this.$store.workerhours.collection.get(this.id)
-
-      const [ checkIn, checkInPostfix ] = moment(this.form.checkIn).format('hh:mm A').split(' ')
-      this.checkIn = checkIn
-      this.checkInPostfix = checkInPostfix
-
-      const [ checkOut, checkOutPostfix ] = moment(this.form.checkOut).format('hh:mm A').split(' ')
-      this.checkOut = checkOut
-      this.checkOutPostfix = checkOutPostfix
     }
     this.loading = false
   },
+  watch: {
+    form: {
+      handler () { this.error = null },
+      deep: true
+    }
+  },
   data () {
     return {
-      tpTimestamp: null,
-      checkIn: null,
-      checkInPostfix: 'AM',
-      checkOut: null,
-      checkOutPostfix: 'AM',
       loading: true,
       savePending: false,
       error: null,
@@ -115,28 +85,18 @@ export default {
     }
   },
   computed: {
-    hours () {
-      // Generate times between 01:00 and 12:00
-      return [...Array(12).keys()].map(x => {
-        return ((x + 1) + ':00').padStart(5, '0')
-      })
-    },
-    checkOutTimestamp () {
-      return moment(moment().format('YYYY-MM-DD') + ' ' + this.checkOut + ' ' + this.checkOutPostfix, 'YYYY-MM-DD hh:mm A')
-    },
-    checkInTimestamp () {
-      return moment(moment().format('YYYY-MM-DD') + ' ' + this.checkIn + ' ' + this.checkInPostfix, 'YYYY-MM-DD hh:mm A')
-    },
     total () {
-      const validDates = this.checkOutTimestamp.isValid() && this.checkInTimestamp.isValid()
-      return validDates ? (this.checkOutTimestamp - this.checkInTimestamp) / 3600000 : 0
+      const { checkIn, checkOut } = this.form
+      return checkIn && checkOut ? (checkOut - checkIn) / 3600000 : 0
     }
   },
   methods: {
     validate () {
-      if (!this.form.worker) this.error = 'You must select Worker'
-      else if (!this.checkInTimestamp.isValid()) this.error = 'You must select when the worker came IN'
-      else if (!this.checkOutTimestamp.isValid()) this.error = 'You must select when the worker went OUT'
+      const { worker, checkIn, checkOut } = this.form
+
+      if (!worker) this.error = 'You must select Worker'
+      else if (!checkIn) this.error = 'You must select when the worker came IN'
+      else if (!checkOut) this.error = 'You must select when the worker went OUT'
       else if (this.total <= 0) this.error = 'Time OUT must be greater then IN'
 
       return this.error || this.save()
@@ -145,9 +105,6 @@ export default {
       this.savePending = true
       await this.$store.workerhours.collection.save({
         ...this.form,
-        notes: this.form.notes || '',
-        checkIn: new Date(this.checkInTimestamp),
-        checkOut: new Date(this.checkOutTimestamp),
         timestamp: this.form.timestamp || new Date()
       })
 
